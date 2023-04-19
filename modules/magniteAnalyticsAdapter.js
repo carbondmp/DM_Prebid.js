2911a48b5006cd9985d1a06be9b1d38b412fe652
Robert Ray Martinez III <rrmartinez1552@gmail.com>
2023-04-19 11:26:53 -0700

    Update magniteAnalyticsAdapter.js

--- modules/magniteAnalyticsAdapter.js
+++ modules/magniteAnalyticsAdapter.js
@@ -93,7 +93,7 @@ const resetConfs = () => {
       vendors: [],
       waitForAuction: true
     },
-    pbaBrowserLocation: 'clientBrowser'
+    pbaBrowserLocation: 'client.browser'
   }
 }
 resetConfs();
